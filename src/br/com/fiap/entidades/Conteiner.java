53d6b3977a08467de0faf7594712a552c8f7e61a
Igor Pereira <igorpereira120@gmail.com>
2021-04-07 00:54:14 -0300

    Create Conteiner entity

---
+++ src/br/com/fiap/entidades/Conteiner.java
@@ -0,0 +1,28 @@
+package br.com.fiap.entidades;
+
+public class Conteiner {
+	
+	private int id;
+	
+	private int contMove;
+
+	public Conteiner() {}
+	
+	public Conteiner(int id) {
+		this.id = id;
+		this.contMove = 0;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getContMove() {
+		return contMove;
+	}
+
+	public void setContMove(int contMove) {
+		this.contMove = contMove;
+	}
+
+}
